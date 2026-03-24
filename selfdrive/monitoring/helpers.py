ba881c86bb979f87a262fa101c8e29e2ec4c8757
bluefacedthief <bluefacedthief@gmail.com>
2026-03-23 17:02:09 -0700

    extra time

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -24,9 +24,9 @@ class DRIVER_MONITOR_SETTINGS:
     self._AWARENESS_TIME = 30. # passive wheeltouch total timeout
     self._AWARENESS_PRE_TIME_TILL_TERMINAL = 15.
     self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-    self._DISTRACTED_TIME = 11. # active monitoring total timeout
-    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+    self._DISTRACTED_TIME = 500004. # active monitoring total timeout
+    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 500002.
+    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 500000.
 
     self._FACE_THRESHOLD = 0.5
     self._EYE_THRESHOLD = 0.45
@@ -57,7 +57,7 @@ class DRIVER_MONITOR_SETTINGS:
     self._POSESTD_THRESHOLD = 0.3
     self._HI_STD_FALLBACK_TIME = int(10  / self._DT_DMON)  # fall back to wheel touch if model is uncertain for 10s
     self._DISTRACTED_FILTER_TS = 0.25  # 0.6Hz
-    self._ALWAYS_ON_ALERT_MIN_SPEED = 80
+    self._ALWAYS_ON_ALERT_MIN_SPEED = 7
 
     self._POSE_CALIB_MIN_SPEED = 13  # 30 mph
     self._POSE_OFFSET_MIN_COUNT = int(60 / self._DT_DMON)  # valid data counts before calibration completes, 1min cumulative
